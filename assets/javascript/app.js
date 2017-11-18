e3910fb849b04b4a68f418cdb85903b6d77a23f5
erictenenbaum <erictenenbaum10@gmail.com>
2017-11-18 11:11:37 -0700

    having trouble with DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,58 @@
+var questionDOM = $("#question");
+var answerOneDOM = $("#answerOne");
+var answerTwoDOM = $("#answerTwo");
+var answerThreeDOM = $("#answerThree");
+var answerFourDOM = $("#answerFour");
+var clear;
+var correctAnswerCounter;
+var incorrectAnswerCounter;
+
 var questionBank = [
 {
 	question: "Who was the first president of the United States?",
 	answerChoices: ["George Washington", "Bill Clinton", "Abe Lincoln", "Jim Carey"],
-	correctAnswerIndex: 0,
+	correctAnswerIndex: "George Washington",
 },
 {
 	question: "Who won the 2017 World Series?",
 	answerChoices: ["Dodgers", "Yankees", "Astros", "Indians"],
-	correctAnswerIndex: 2,
+	correctAnswerIndex: "Astros",
 },
 {
 	question: "What year did the Angels win the World Series?",
 	answerChoices: ["2000", "2001", "2002", "2003"],
-	correctAnswerIndex: 2,
+	correctAnswerIndex: "2002",
 },
 {
 	question: "Which direction does the sun rise?",
 	answerChoices: ["North", "East", "South", "West"],
-	correctAnswerIndex: 1,
+	correctAnswerIndex: "East",
 },
 {
 	question: "What year did the Diamondbacks win the World Series?",
 	answerChoices: ["2000", "2001", "2002", "2003"],
-	correctAnswerIndex: 1,
+	correctAnswerIndex: "2001",
 },
 {
 	question: "What was the last year the Lakers won the NBA Finals?",
 	answerChoices: ["2007", "2008", "2009", "2010"],
-	correctAnswerIndex: 3,
+	correctAnswerIndex: "2010",
 },
 {
 	question: "What is the capital of Texas?",
 	answerChoices: ["Houston", "Dallas", "San Antonio", "Austin"],
-	correctAnswerIndex: 3,
+	correctAnswerIndex: "Austin",
 },
 {
 	question: "What is the highest single-game point total of Kobe's Career?",
 	answerChoices: ["60", "69", "81", "95"],
-	correctAnswerIndex: 2,
+	correctAnswerIndex: "81",
 },
 {
 	question: "How many championships have the Lakers won?",
 	answerChoices: ["15", "16", "17", "18"],
-	correctAnswerIndex: 1,
-}]
+	correctAnswerIndex: "16",
+}];
 
 var trivia = {	
 
@@ -51,126 +60,134 @@ var trivia = {
 
 	initializeGame: function() {
 		trivia.j = 0;
+		correctAnswerCounter = 0;
+		incorrectAnswerCounter = 0;
 
 		trivia.questionOne(questionBank[trivia.j].question, 
 			questionBank[trivia.j].answerChoices[0], 
 			questionBank[trivia.j].answerChoices[1],
 			questionBank[trivia.j].answerChoices[2], 
-			questionBank[trivia.j].answerChoices[3], 
-			questionBank[trivia.j].correctAnswerIndex);
+			questionBank[trivia.j].answerChoices[3]
+			);
 
 		trivia.timer();
 	},
 
-	questionOne: function(q, a, b, c, d, e) {	
 
-		var userGuess;
+	nextQuestion: function() {
 
-
-		$("#question").html(q);
-		$("#answerOne").html(a).on("click", function() {
-
-			if (e === 0) {
-				console.log("true");			
-
-			}
-			else {
-				console.log("false");
-			}	
-
-			trivia.nextQuestion();					
-
-		});
-		$("#answerTwo").html(b).on("click", function() {
-			if (e === 1) {
-				console.log("true");
-			}
-			else {
-				console.log("false");
-			}	
-			
-			trivia.nextQuestion();			
-
-		});
-		$("#answerThree").html(c).on("click", function() {
-			if (e === 2) {
-				console.log("true");
-			}
-			else {
-				console.log("false");
-			}	
-
-			trivia.nextQuestion();				
-			
-		});
-		$("#answerFour").html(d).on("click", function() {
-			if (e === 3) {
-				console.log("true");
-			}
-			else {
-				console.log("false");
-			}
-
-			trivia.nextQuestion();						
-		});	
-
-	// if (trivia.j < questionBank.length -1) {
-	// 	trivia.nextQuestion();
-	// 	console.log("still on");
-	// }
-	// else {
-	// 	console.log("game over");
-	// }	
-},
-
-nextQuestion: function() {
-
-	// if (trivia.j < questionBank.length - 1) {
-	// 	trivia.j++
-	// }
-	
-	trivia.j++;
-	trivia.questionOne(questionBank[trivia.j].question, 
-			questionBank[trivia.j].answerChoices[0], 
-			questionBank[trivia.j].answerChoices[1],
-			questionBank[trivia.j].answerChoices[2], 
-			questionBank[trivia.j].answerChoices[3], 
-			questionBank[trivia.j].correctAnswerIndex);
-
-	trivia.timer();
-},
-
-timer: function() {
-	setTimeout(function() {
-
-		if(trivia.j < questionBank.length -1) {
-
-			trivia.nextQuestion();
-
+		if (trivia.j < questionBank.length - 1) {
+			// trivia.j++
+			console.log("continue");
 		}
 		else {
 			alert("thats it!");
 		}
+		clearInterval(clear)
+		trivia.j++;
+		trivia.questionOne(questionBank[trivia.j].question, 
+				questionBank[trivia.j].answerChoices[0], 
+				questionBank[trivia.j].answerChoices[1],
+				questionBank[trivia.j].answerChoices[2], 
+				questionBank[trivia.j].answerChoices[3]
+			);
 
-		}, 1000 * 10);
+		trivia.timer();
+	},
+
+	timer: function() {
+		clear = setTimeout(function() {
+
+			if(trivia.j < questionBank.length -1) {
+
+				trivia.nextQuestion();
+
+			}
+			else {
+				alert("thats it!");
+			}
+
+			}, 10000);
+	},
+	questionOne: function (q, a, b, c, d) {
+
+		var userGuess;
+		questionDOM.html(q)
+		answerOneDOM.html(a).attr('data-value', a);
+		answerTwoDOM.html(b).attr('data-value', b);
+		answerThreeDOM.html(c).attr('data-value', c);
+		answerFourDOM.html(d).attr('data-value', d);
+		// $("#answerThree").html(e) // to handle e param
+	},
+	displayPage: function() {
+
+		$("#container").html("display page");
+		
+		setTimeout(function() {
+			// $("#container").html("new page")
+			trivia.nextQuestion();
+		}, 3000)
+		// clearInterval();
+
+		
+			// trivia.questionOne();
 	}
+
 }
 
-
-trivia.initializeGame();
-
 $("#startButton").on("click", function() {
-		trivia.nextQuestion();
+	trivia.initializeGame();
 		console.log("hi");
-	})
+})
+
+$("#answerOne").on("click", function () {
+
+	if (questionBank[trivia.j].correctAnswerIndex === $(this).attr('data-value')) {
+		console.log("true");		
+		// trivia.nextQuestion();
+		trivia.displayPage();
+			
+	}
+	else {
+		console.log("false");
+		trivia.nextQuestion();
+	}
+
+});
+$("#answerTwo").on("click", function () {
+	if (questionBank[trivia.j].correctAnswerIndex === $(this).attr('data-value')) {
+		console.log("true");
+		trivia.nextQuestion();
+	}
+	else {
+		console.log("false");
+		trivia.nextQuestion();
+	}
+
+});
+$("#answerThree").on("click", function () {
+	if (questionBank[trivia.j].correctAnswerIndex === $(this).attr('data-value')) {
+		console.log("true");
+		trivia.nextQuestion();
+	}
+	else {
+		console.log("false");
+		trivia.nextQuestion();
+	}
 
 
 
+})
+$("#answerFour").on("click", function () {
+	if (questionBank[trivia.j].correctAnswerIndex === $(this).attr('data-value')) {
+		console.log("true");
+		trivia.nextQuestion();
+	}
+	else {
+		console.log("false");
+		trivia.nextQuestion();
+	}
 
-
-
-
-
-
+})
 
 
